7cb2b28d9a0cabf7a2d048ff8b15c493750f8fc9
Vaclav Mach <vaclavmach@hotmail.cz>
2025-05-04 11:35:31 +0200

    cleanup, button reading

--- code/sw63-rust/src/main.rs
+++ code/sw63-rust/src/main.rs
@@ -1,22 +1,22 @@
 #![no_std]
 #![no_main]
 
-use panic_halt as _;
-use cortex_m_rt::entry;
 use atsamd_hal::{
     clock::GenericClockController,
     delay::Delay,
+    gpio::v2::{Input, Output, Pin, Pins, PullUp, PushPull, PA04, PA23},
     prelude::*,
     target_device::Peripherals,
-    gpio::v2::{Pin, PA23, Output, PushPull},
 };
+use cortex_m_rt::entry;
+use panic_halt as _;
 
 #[entry]
 fn main() -> ! {
     // Get peripherals
     let mut peripherals = Peripherals::take().unwrap();
     let core = cortex_m::Peripherals::take().unwrap();
-    
+
     // Configure clocks using the internal oscillator
     let mut clocks = GenericClockController::with_internal_32kosc(
         peripherals.GCLK,
@@ -24,17 +24,23 @@ fn main() -> ! {
         &mut peripherals.SYSCTRL,
         &mut peripherals.NVMCTRL,
     );
-    
-    // Setup delay provider 
+
+    // Setup delay provider
     let mut delay = Delay::new(core.SYST, &mut clocks);
-    
+
     // Configure pins - PA23 as output pin for the LED
-    let pins = atsamd_hal::gpio::v2::Pins::new(peripherals.PORT);
+    let pins = Pins::new(peripherals.PORT);
+
     let mut led_pin: Pin<PA23, Output<PushPull>> = pins.pa23.into_push_pull_output();
-    
+    let button_pin: Pin<PA04, Input<PullUp>> = pins.pa04.into_pull_up_input();
+
     // Main loop - blink LED
     loop {
         led_pin.toggle().unwrap();
-        delay.delay_ms(50u32);
+        let mut delay_time = 500u32;
+        if button_pin.is_low().unwrap() {
+            delay_time = 50u32;
+        }
+        delay.delay_ms(delay_time);
     }
 }

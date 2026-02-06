6faba244bbf93539ab40a14180d4487e1e2f7e4e
Vaclav Mach <vaclavmach@hotmail.cz>
2026-02-06 23:41:00 +0100

    remove double press (was too complicated)

--- code/src/app/ui/Layer.hpp
+++ code/src/app/ui/Layer.hpp
@@ -11,11 +11,10 @@ public:
     };
     enum class Event
     {
-        SHORT_PRESS,  // pressed and released under 2 seconds
-        MEDIUM_PRESS, // hold over 2 seconds
-        LONG_PRESS,   // hold over 5 seconds
-        DOUBLE_PRESS, // two quick presses under 0.5 seconds
-        MULTI_PRESS,  // five quick presses under 2.5 seconds
+        SHORT_PRESS,
+        MEDIUM_PRESS,
+        LONG_PRESS,
+        MULTI_PRESS,
         ENTER,
         LEAVE
     };

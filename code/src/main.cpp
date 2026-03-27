3b1fd0554c8df71c76a1105b47e2bb1a603866af
Vaclav Mach <vaclavmach@hotmail.cz>
2026-03-27 21:23:52 +0100

    main lincense and cleanup

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2026 Vaclav Mach (xx0x)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include "app/App.hpp"
 #include "dev/System.hpp"
 #include "stm32l052xx.h"
@@ -66,8 +90,6 @@ extern "C" void EXTI0_1_IRQHandler(void)
         // Clear the interrupt flag
         __HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_0);
 
-        // Set button pressed flag?
-
         // The system will automatically exit from stop mode
         // No additional code needed here for basic wakeup
     }
@@ -75,16 +97,16 @@ extern "C" void EXTI0_1_IRQHandler(void)
 
 extern "C" void EXTI4_15_IRQHandler()
 {
+    // Charge state change
     if (__HAL_GPIO_EXTI_GET_IT(GPIO_PIN_8) != RESET)
     {
         __HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_8);
-        // Handle charge state change
     }
-    
+
+    // Power detect (power plugged in)
     if (__HAL_GPIO_EXTI_GET_IT(GPIO_PIN_15) != RESET)
     {
         __HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_15);
-        // Handle power detect (power plugged in)
     }
 }
 
